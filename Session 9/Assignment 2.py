eafd9c517690e59a1e247e36ba27d4da1a1d5794
lynnvmara <71988371+lynnvmara@users.noreply.github.com>
2020-11-16 19:29:07 -0600

    Update Assignment 2.py

--- Session 9/Assignment 2.py
+++ Session 9/Assignment 2.py
@@ -1,6 +1,6 @@
 def displayArray(name):
   for offset in range(0, 9 + 1, 1):
-    print(name[offset] + "'s score is " + score[offset] + "%.")
+    print(name[offset] + "'s score is " + str(score[offset]) + "%.")
 
 name = [""] * (10)
 score = [""] * (10)
@@ -15,14 +15,14 @@ name[6] = "Meyers"
 name[7] = "Sheridan"
 name[8] = "Schafernak"
 name[9] = "Joyce"
-score[0] = "100"
-score[1] = "90"
-score[2] = "80"
-score[3] = "70"
-score[4] = "60"
-score[5] = "50"
-score[6] = "40"
-score[7] = "30"
-score[8] = "20"
-score[9] = "10"
+score[0] = 100
+score[1] = 90
+score[2] = 80
+score[3] = 70
+score[4] = 60
+score[5] = 50
+score[6] = 40
+score[7] = 30
+score[8] = 20
+score[9] = 10
 displayArray(name)
